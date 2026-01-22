6331071f6a6b153bfdc9b585335bd40cb4ba3946
LuSchumacher <lu.schumacher@bluewin.ch>
2026-01-22 20:45:20 +0100

    add helpers.py

---
+++ superstats/utils/__init__.py
@@ -0,0 +1,1 @@
+from .helpers import scaled_sigmoid
---
+++ superstats/utils/helpers.py
@@ -0,0 +1,13 @@
+import numpy as np
+from numba import njit
+
+@njit
+def bounded_sigmoid(
+    x: float | np.ndarray,
+    lower_bound: float | np.ndarray,
+    upper_bound: float | np.ndarray
+) -> float:
+    """
+    Apply a sigmoid transformation and rescale to a bounded interval.
+    """
+    return lower_bound + (upper_bound - lower_bound) / (1.0 + np.exp(-x))
