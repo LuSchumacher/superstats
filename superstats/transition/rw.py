e21f76c3699f60a805cb4e4aba5935dde8a1a8b0
LuSchumacher <lu.schumacher@bluewin.ch>
2026-01-23 09:28:03 +0100

    improve first transition model

--- superstats/transition/rw.py
+++ superstats/transition/rw.py
@@ -1,14 +1,14 @@
-from typing import Callable, Tuple, Union
+from typing import Tuple
 import numpy as np
-from numba import njit, prange
+from numba import njit
 from superstats.utils.helpers import scaled_sigmoid
 from .transition import Transition
+from prior.prior import Prior
 
 @njit
 def sample_rw(
     theta: np.ndarray,
     eta: np.ndarray,
-    batch_size: int,
     steps: int,
     bounds: np.ndarray,
     transform: bool = False
@@ -16,17 +16,17 @@ def sample_rw(
     """
     Generates a single transition from a Gaussian random walk.
     """
-    z = np.random.randn(batch_size, steps-1)
-    for b in prange(batch_size):
-        if transform:
-            for t in range(1, steps):
-                theta[b, t] = theta[b, t-1] + eta[b] * z[b, t-1]
-            theta[b, :] = scaled_sigmoid(
-                theta[b, :], bounds[0], bounds[1]
-            )
-        else:
-            theta[b, :] = np.minimum(
-                np.maximum(theta[b, :], bounds[0]),
+    z = np.random.randn(steps-1)
+    if transform:
+        for t in range(1, steps):
+            theta[t] = theta[t-1] + eta * z[t-1]
+        theta[:] = scaled_sigmoid(
+            theta, bounds[0], bounds[1]
+        )
+    else:
+        for t in range(1, steps):
+            theta[t] = np.minimum(
+                np.maximum(theta[t-1] + eta * z[t-1], bounds[0]),
                 bounds[1]
             )
     return theta
@@ -35,10 +35,10 @@ def sample_rw(
 class RW(Transition):
     def __init__(
         self,
-        initial_prior: Callable,
-        bounds: Union[Tuple[float, float], np.ndarray],
+        bounds: Tuple[float, float] | np.ndarray,
+        initial_prior: None | Prior = None,
         sigma: float = 0.1,
-        transform: bool = True
+        transform: bool = False
     ):
         super().__init__(
             initial_prior=initial_prior,
@@ -47,15 +47,15 @@ class RW(Transition):
         )
         self.sigma = sigma
 
-    def sample(self, batch_size: int, steps: int) -> np.ndarray:
-        theta = np.zeros((batch_size, steps), dtype=np.float32)
-        theta[:, 0] = self.initial_prior.sample(batch_size)
-        eta = np.abs(np.random.normal(0, self.sigma, size=(batch_size, )))
-        return sample_rw(
+    def sample(self, steps: int) -> dict:
+        theta = np.zeros((steps), dtype=np.float32)
+        theta[0] = self.initial_prior.sample()
+        sigma = np.abs(np.random.normal(0.0, self.sigma))
+        theta[:] = sample_rw(
             theta,
-            eta,
-            batch_size,
+            sigma,
             steps,
             self.bounds,
             self.transform
         )
+        return {"local_param": theta, "sigma": sigma}
--- superstats/transition/transition.py
+++ superstats/transition/transition.py
@@ -1,18 +1,24 @@
 from abc import ABC, abstractmethod
-from typing import Callable, Tuple, Union
+from typing import Callable, Tuple
+from prior.prior import Prior
 import numpy as np
 
 class Transition(ABC):
     def __init__(
         self,
-        initial_prior: Callable,
-        bounds: Union[Tuple[float, float], np.ndarray],
-        transform: bool = True
+        bounds: Tuple[float, float] | np.ndarray,
+        initial_prior: None | Prior = None,
+        transform: bool = False
     ):
-        self.initial_prior = initial_prior
+        if initial_prior is None:
+            self.initial_prior = Prior(
+                "uniform", low=bounds[0], high=bounds[1]
+            )
+        else:
+            self.initial_prior = initial_prior
         self.bounds = np.asarray(bounds, dtype=np.float32)
         self.transform = transform
     
     @abstractmethod
-    def sample(self, batch_size: int, steps: int) -> np.ndarray:
+    def sample(self, steps: int) -> np.ndarray:
         pass
