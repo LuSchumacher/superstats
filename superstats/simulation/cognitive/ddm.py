17e195e557bed2b4f7f136cb43e05107570824ef
LuSchumacher <lu.schumacher@bluewin.ch>
2026-03-28 15:37:49 +0100

    tweak ddm

--- superstats/simulation/cognitive/ddm.py
+++ superstats/simulation/cognitive/ddm.py
@@ -1,36 +1,6 @@
 import numpy as np
 from numba import njit, prange
 
-# @njit
-# def sample_ddm(
-#     v: np.ndarray,
-#     a: np.ndarray,
-#     tau: np.ndarray,
-#     bias: np.ndarray,
-#     sigma: float = 1.0,
-#     dt: float = 0.001,
-#     max_steps: int = 10000,
-# ):
-#     c = sigma * np.sqrt(dt)
-#     num_steps = v.shape[0]
-#     data = np.empty((num_steps, 2), dtype=np.float32)
-
-#     for i in prange(num_steps):
-#         x = bias[i] * a[i]
-#         t = tau[i]
-#         for _ in range(max_steps):
-#             t += dt
-#             x += v[i] * dt + c * np.random.normal()
-#             if x >= a[i]:
-#                 data[i] = np.array([t, 1.0], dtype=np.float32)
-#             elif x <= -a[i]:
-#                 data[i] = np.array([t, 0.0], dtype=np.float32)
-#             else:
-#                 # No decision within max_steps
-#                 data[i] = np.array([-1.0, -1.0], dtype=np.float32)
-    
-#     return data
-
 @njit(parallel=True, fastmath=True)
 def sample_ddm(
     v: np.ndarray,
