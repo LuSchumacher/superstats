61a91aa0633a3b5db316b4af3f3c3aae40ecf282
LuSchumacher <lu.schumacher@bluewin.ch>
2026-04-23 11:30:03 +0200

    update inits

--- superstats/transition/__init__.py
+++ superstats/transition/__init__.py
@@ -1,14 +1,9 @@
 from .random_walk import RandomWalk
-from .random_walk_drift import RandomWalkDrift
 from .auto_regression import AutoRegression
-from .auto_regression_drift import AutoRegressionDrift
-
 from .transition import Transition
 
 __all__ = [
     "RandomWalk",
-    "RandomWalkDrift",
     "AutoRegression",
-    "AutoRegressionDrift",
     "Transition"
 ]
