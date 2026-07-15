acb924fc0d403bc939fc8531a109c8441f126671
LuSchumacher <lu.schumacher@bluewin.ch>
2026-07-15 09:04:19 -0400

    fix init

--- superstats/simulation/__init__.py
+++ superstats/simulation/__init__.py
@@ -2,5 +2,12 @@
 
 from .generative_model import GenerativeModel
 from .cognitive.ddm import sample_ddm
+from .cognitive.rdm import sample_rdm
+from .cognitive.cdm import sample_cdm
 
-__all__ = ["GenerativeModel", "sample_ddm"]
+__all__ = [
+    "GenerativeModel",
+    "sample_ddm",
+    "sample_rdm",
+    "sample_d]cdm",
+]
